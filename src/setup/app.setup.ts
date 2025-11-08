befbc040177faaa4bfe9c7ba9bf56b1b5d4b379b
StepanAndrosov <stepan.news@yandex.ru>
2025-11-08 14:18:38 +0300

    fix prefix

--- src/setup/app.setup.ts
+++ src/setup/app.setup.ts
@@ -1,8 +1,6 @@
 import { INestApplication } from '@nestjs/common';
-import { globalPrefixSetup } from './global-prefix.setup';
 import { swaggerSetup } from './swagger.setup';
 
 export function appSetup(app: INestApplication) {
-  globalPrefixSetup(app);
   swaggerSetup(app);
 }
--- src/setup/global-prefix.setup.ts
+++
@@ -1,7 +0,0 @@
-import { INestApplication } from '@nestjs/common';
-
-export const GLOBAL_PREFIX = 'api';
-
-export function globalPrefixSetup(app: INestApplication) {
-  app.setGlobalPrefix(GLOBAL_PREFIX);
-}
