d221fef4ea04750b085f88cd86e536adb0a435b2
StepanAndrosov <stepan.news@yandex.ru>
2025-12-21 09:19:28 +0300

    cors for 3000

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,12 @@ import { appSettings } from './setup/app-settings';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   appSetup(app);
-  app.enableCors();
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
   await app.listen(appSettings.api.APP_PORT, () => {
     console.log('App starting listen port: ', appSettings.api.APP_PORT);
     console.log('ENV: ', appSettings.env.getEnv());
