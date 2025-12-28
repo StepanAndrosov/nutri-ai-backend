0638f21b24817236fac6b982765a9f6d2437ae30
StepanAndrosov <stepan.news@yandex.ru>
2025-12-28 10:00:43 +0300

    jwt quard update

--- src/modules/auth/api/guards/jwt-auth.guard.ts
+++ src/modules/auth/api/guards/jwt-auth.guard.ts
@@ -1,10 +1,23 @@
 import { Injectable, ExecutionContext } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Observable } from 'rxjs';
+import { DomainException } from '../../../../core/exceptions/domain-exceptions';
+import { DomainExceptionCode } from '../../../../core/exceptions/domain-exception-codes';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
   canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
     return super.canActivate(context);
+  }
+
+  handleRequest(err: any, user: any, _info: any) {
+    // If there's an error or no user, throw DomainException with Unauthorized code
+    if (err || !user) {
+      throw new DomainException({
+        code: DomainExceptionCode.Unauthorized,
+        message: 'Unauthorized',
+      });
+    }
+    return user;
   }
 }
